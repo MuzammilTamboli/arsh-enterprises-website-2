8d7d7c6fe8347ce3f7110d2cb828ba04f6db327a
Muzammiltmaboli <muzammiltamboli04@gmail.com>
2025-08-08 22:14:01 +0530

    Fix vite config for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,11 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/arsh-enterprises-website-2/',  
+  base: '/arsh-enterprises-website-2/',  // 👈 Repo name
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
 });
